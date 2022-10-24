bb5a618f3a5521b53deca2c8a2aab64db95c22a5
sachin <sachinkaru.tm@gmail.com>
2022-10-24 09:59:18 +0530

    errorFixed

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/', function () {
     return view('Home');
 });
 
-Route::get('/home2','interfaceController@indexHome2');
+Route::get('/home2','InterfaceController@indexHome2');
 
 Route::get('/home3', function () {
     return view('Home03');
